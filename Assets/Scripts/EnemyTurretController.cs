33dc358144e55c419f4a1c61c25faaf8ff78e267
kadynCBR <kadyn_martinez@hotmail.com>
2023-01-31 17:00:18 -0800

    Added turret type.

---
+++ Assets/Scripts/EnemyTurretController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurretController : MonoBehaviour
+{
+    public Transform playerTrans;
+    public float attackRange;
+    public Transform turretTransform;
+
+    [GradientUsageAttribute(true)]
+    public Gradient onHitBodyGradient;
+
+    public float flashOnHitDuration = 0.5f;
+    void Start()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform.Find("LockOnSnap");
+    }
+
+    void FixedUpdate()
+    {
+        // Look at
+        turretTransform.LookAt(playerTrans, Vector3.up);
+    }
+
+    public void OnTakeDamage()
+    {
+        StartCoroutine(DamageBlink(1));
+    }
+
+    IEnumerator DamageBlink(int blinkCount)
+    {
+        Material mat = GetComponentInChildren<Renderer>().material;
+        for (int i = 0; i < blinkCount; i++)
+        {
+            float blink_start = Time.time;
+            while (Time.time - blink_start < flashOnHitDuration)
+            {
+                Color currentColor = onHitBodyGradient.Evaluate((Time.time - blink_start) / flashOnHitDuration);
+                mat.SetColor("_EmissionColor", currentColor);
+                yield return null;
+            }
+        }
+        mat.SetColor("_EmissionColor", Color.black);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+}
