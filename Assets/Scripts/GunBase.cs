bff46495f7b722641d5b65f82eb8a4a38b758fcd
kadynCBR <kadyn_martinez@hotmail.com>
2023-02-03 12:23:11 -0800

    sound and juice.

--- Assets/Scripts/GunBase.cs
+++ Assets/Scripts/GunBase.cs
@@ -1,46 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GunBase : MonoBehaviour
 {
 
     public float damage, knockBack, fireRate, speed, explosionPower, TTL;
-    private float timer; 
-    public bool explosive, isFriendly, piercing, canFire; 
-    public GameObject bullet;  
+    private float timer;
+    public bool explosive, isFriendly, piercing, canFire;
+    public GameObject bullet;
+    public UnityEvent OnShotCreated;
 
 
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime; 
-        if(timer > fireRate){
-            canFire = true; 
+        timer += Time.deltaTime;
+        if (timer > fireRate)
+        {
+            canFire = true;
         }
     }
 
-    public void CreateBullet(Vector3 pos, Quaternion rot) {
+    public void CreateBullet(Vector3 pos, Quaternion rot)
+    {
         GameObject go = Instantiate(bullet, pos, rot);
         BulletBase bb = go.GetComponent<BulletBase>();
-        bb.damage = damage; 
-        bb.explosionPower = explosionPower; 
-        bb.speed = speed; 
-        bb.knockBack = knockBack; 
+        bb.damage = damage;
+        bb.explosionPower = explosionPower;
+        bb.speed = speed;
+        bb.knockBack = knockBack;
         bb.TTL = TTL;
-        bb.piercing = piercing; 
-        bb.explosive = explosive;   
+        bb.piercing = piercing;
+        bb.explosive = explosive;
         bb.isFriendly = isFriendly;
     }
 
-    public void OnShoot(){
-        if(!canFire){
-            return; 
+    public void OnShoot()
+    {
+        if (!canFire)
+        {
+            return;
         }
         CreateBullet(transform.position, transform.rotation);
-        timer = 0; 
-        canFire = false; 
+        OnShotCreated.Invoke();
+        timer = 0;
+        canFire = false;
     }
 
 
